cce931fded6d40bf6b8a5ee2c33b990958e1125b
Mano Ekambaram <digitaldeliver@gmail.com>
2023-10-16 08:37:32 +0530

    express_second_Lets get started with Express

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require('http');
+
 const express = require('express');
+
 const app = express()
-const routes = require('./routes')
-const server = http.createServer(app)
-server.listen(3000);
+
+app.use((req,res,next)=>{
+    console.log("inside first middleware")
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("inside last middleware")
+    res.send('<h1>Hello from Express</h1>')
+})
+app.listen(3000);
